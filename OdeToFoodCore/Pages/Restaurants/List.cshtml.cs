a81e9cd5443e4fc975f638c862a5af7c6d192302
Wesley Dwyer <dwes_deomar@hotmail.com>
2019-12-31 15:39:19 -0500

    Display List of restaurants from in memory data source

--- OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
+++ OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFoodCore.Pages.Restuarants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
-
-        public ListModel(IConfiguration config)
+        private readonly IRestaurantData restaurantData;
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
         public string Message { get; set; }
         public void OnGet()
         {
             //Message = "Hello, World!";
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
